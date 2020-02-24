08a3072d0e2e323c209da50fce1d6281fc610e3a
Peter de Boer <peterdeboer_private@hotmail.com>
2020-02-24 08:38:14 +0000

    add scripts to start zookeeper and kafka server

---
+++ kaf.sh
@@ -0,0 +1,2 @@
+echo "/usr/bin/kafka-server-start config/server.properties"
+/usr/bin/kafka-server-start config/server.properties
---
+++ zoo.sh
@@ -0,0 +1,2 @@
+echo "/usr/bin/zookeeper-server-start config/zookeeper.properties"
+/usr/bin/zookeeper-server-start config/zookeeper.properties
