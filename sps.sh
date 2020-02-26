cb6d771afe734bda216e1801753202e34a51828a
Peter de Boer <peterdeboer_private@hotmail.com>
2020-02-26 12:22:19 +0000

    script to execute spark-submit

---
+++ sps.sh
@@ -0,0 +1,2 @@
+echo "spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py"
+spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
